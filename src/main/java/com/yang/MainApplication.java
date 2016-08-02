f8f2f6909cdd5a4b4686c7453fa4ed74d4252681
lucifer7 <skyvoice@foxmail.com>
2016-08-02 17:59:39 +0800

    spring boot admin, tbc

--- src/main/java/com/yang/MainApplication.java
+++ src/main/java/com/yang/MainApplication.java
@@ -1,9 +1,11 @@
 package com.yang;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication  /* 该应用程序入口的标志 */    // or use below instead
+@EnableAdminServer      /* Spring boot admin */
 //@ComponentScan    //组件扫描，自动发现和装配bean
 //@Configuration    //使用Java文件配置，类似xml
 //@EnableAutoConfiguration  //自动配置
